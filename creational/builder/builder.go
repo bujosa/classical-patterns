f3c45df2fcf23040f2ea24250e18903bc83b43b6
bujosa <davidbujosa@gmail.com>
2021-09-25 01:06:32 -0400

    Add example about builder pattern

---
+++ creational/builder/builder.go
@@ -0,0 +1,76 @@
+package builder
+
+type BuildProcess interface {
+	SetWheels() BuildProcess
+	SetSeats() BuildProcess
+	SetStructure() BuildProcess
+	GetVehicle() VehicleProduct
+}
+
+//Director
+type ManufacturingDirector struct {
+	builder BuildProcess
+}
+
+func (f *ManufacturingDirector) Construct() {
+	f.builder.SetSeats().SetStructure().SetWheels()
+}
+
+func (f *ManufacturingDirector) SetBuilder(b BuildProcess) {
+	f.builder = b
+}
+
+//Product
+type VehicleProduct struct {
+	Wheels    int
+	Seats     int
+	Structure string
+}
+
+//A Builder of type car
+type CarBuilder struct {
+	v VehicleProduct
+}
+
+func (c *CarBuilder) SetWheels() BuildProcess {
+	c.v.Wheels = 4
+	return c
+}
+
+func (c *CarBuilder) SetSeats() BuildProcess {
+	c.v.Seats = 5
+	return c
+}
+
+func (c *CarBuilder) SetStructure() BuildProcess {
+	c.v.Structure = " "
+	return c
+}
+
+func (c *CarBuilder) GetVehicle() VehicleProduct {
+	return c.v
+}
+
+//A Builder of type Motorbike
+type BikeBuilder struct {
+	v VehicleProduct
+}
+
+func (b *BikeBuilder) SetWheels() BuildProcess {
+	b.v.Wheels = 2
+	return b
+}
+
+func (b *BikeBuilder) SetSeats() BuildProcess {
+	b.v.Seats = 1
+	return b
+}
+
+func (b *BikeBuilder) SetStructure() BuildProcess {
+	b.v.Structure = " "
+	return b
+}
+
+func (b *BikeBuilder) GetVehicle() VehicleProduct {
+	return b.v
+}
