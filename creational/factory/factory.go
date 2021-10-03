3fefe7a37f6394c79036c2441211af7d4373d081
bujosa <davidbujosa@gmail.com>
2021-10-02 20:10:31 -0400

    Add payment method

--- creational/factory/factory.go
+++ creational/factory/factory.go
@@ -1,1 +1,11 @@
 package factory
+
+type PaymentMethod interface {
+	Pay(amount float32) string
+}
+
+// Our current implemented payment methods are described below
+const (
+	Cash      = 1
+	DebitCard = 2
+)
