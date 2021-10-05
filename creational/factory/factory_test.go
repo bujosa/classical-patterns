4c54ea4b8ff991ce26268154af943080ffd56f52
bujosa <davidbujosa@gmail.com>
2021-10-05 12:51:45 -0400

    Add test

---
+++ creational/factory/factory_test.go
@@ -0,0 +1,19 @@
+package factory
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestGetPaymentMethodCash(t *testing.T) {
+	payment, err := GetPaymentMethod(Cash)
+	if err != nil {
+		t.Fatal("A payment method of type 'Cash' must exist")
+	}
+
+	msg := payment.Pay(10.30)
+	if !strings.Contains(msg, "paid using cash") {
+		t.Error("The cash payment method message wasn't correct")
+	}
+	t.Log("LOG:", msg)
+}
