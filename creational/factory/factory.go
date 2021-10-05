22bf5d4c2782a3d3c158c9f7454a4377d03ffb0a
bujosa <davidbujosa@gmail.com>
2021-10-05 13:30:10 -0400

    Finished factory

--- creational/factory/factory.go
+++ creational/factory/factory.go
@@ -1,6 +1,9 @@
 package factory
 
-import "errors"
+import (
+	"errors"
+	"fmt"
+)
 
 type PaymentMethod interface {
 	Pay(amount float32) string
@@ -13,16 +16,23 @@ const (
 )
 
 func GetPaymentMethod(m int) (PaymentMethod, error) {
-	return nil, errors.New("not implemented yet")
+	switch m {
+		case Cash:
+			return new(CashPM), nil
+		case DebitCard:
+			return new(DebitCardPM), nil
+		default:
+			return nil, errors.New(fmt.Sprintf("Payment Method %d not recognized\n", m))
+	}
 }
 
 type CashPM struct{}
 type DebitCardPM struct{}
 
 func (c *CashPM) Pay(amount float32) string {
-	return ""
+	return fmt.Sprintf("%0.2f paid using cash\n", amount)
 }
 
 func (c *DebitCardPM) Pay(amount float32) string {
-	return ""
+	return fmt.Sprintf("%0.2f paid using debit card\n", amount)
 }
