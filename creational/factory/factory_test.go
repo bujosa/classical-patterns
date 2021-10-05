0dd0ed936065cef0c77589a73469779cabfc5da3
bujosa <davidbujosa@gmail.com>
2021-10-05 12:54:01 -0400

    Add another test

--- creational/factory/factory_test.go
+++ creational/factory/factory_test.go
@@ -17,3 +17,16 @@ func TestGetPaymentMethodCash(t *testing.T) {
 	}
 	t.Log("LOG:", msg)
 }
+
+func TestGetPaymentMethodDebit(t *testing.T) {
+	payment, err := GetPaymentMethod(DebitCard)
+	if err != nil {
+		t.Fatal("A payment method of type 'DebitCard' must exist")
+	}
+
+	msg := payment.Pay(22.30)
+	if !strings.Contains(msg, "paid using debit card") {
+		t.Error("The debit card payment method message wasn't correct")
+	}
+	t.Log("LOG:", msg)
+}
