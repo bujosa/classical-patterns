23a640980034de4b151731d7cd0480b9f261ad88
bujosa <davidbujosa@gmail.com>
2021-10-23 22:53:45 -0400

    Add vehicle factory

--- abstract_factory/vehicle_factory.go
+++ abstract_factory/vehicle_factory.go
@@ -1,5 +1,23 @@
 package abstract_factory
 
+import "fmt"
+
 type VehicleFactory interface {
-	NewVehicle(v int) (Vehicle, error)
+	GetVehicle(v int) (Vehicle, error)
+}
+
+const (
+	CarFactoryType       = 1
+	MotorbikeFactoryType = 2
+)
+
+func GetVehicleFactory(f int) (VehicleFactory, error) {
+	switch f {
+	case CarFactoryType:
+		return new(CarFactory), nil
+	case MotorbikeFactoryType:
+		return new(MotorbikeFactory), nil
+	default:
+		return nil, fmt.Errorf("factory with id %d not recognized", f)
+	}
 }
