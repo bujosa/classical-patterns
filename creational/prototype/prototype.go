5c0806a302464ce02b8aab02a8a8433a92c1f5ad
bujosa <davidbujosa@gmail.com>
2021-10-24 23:26:09 -0400

    Add get info

--- creational/prototype/prototype.go
+++ creational/prototype/prototype.go
@@ -17,3 +17,36 @@ type ShirtsCache struct{}
 func (s *ShirtsCache) GetClone(m int) (ItemInfoGetter, error) {
 	return nil, fmt.Errorf("not implemented yet")
 }
+
+type ItemInfoGetter interface {
+	GetInfo() string
+}
+
+type ShirtColor byte
+
+type Shirt struct {
+	Price float32
+	SKU   string
+	Color ShirtColor
+}
+
+func (s *Shirt) GetInfo() string {
+	return fmt.Sprintf("Shirt with SKU '%s' and Color id %d that costs %f\n", s.SKU, s.Color, s.Price)
+}
+
+func GetShirtsCloner() ShirtCloner {
+	var cache ShirtsCache
+	return &cache
+}
+
+func whitePrototype() *Shirt {
+	return &Shirt{
+		Price: 15.00,
+		SKU:   "empty",
+		Color: White,
+	}
+}
+
+func (i *Shirt) GetPrice() float32 {
+	return i.Price
+}
